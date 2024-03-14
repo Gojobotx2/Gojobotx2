528cf1dad3c7c626d3b36a8fe2728ed6438ce433
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-14 23:50:09 +0100

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -1,24 +1,7 @@
 function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator);
-  const contacts = data.map(([id, name]) => [id, name]);
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
-  const taguser = "المستخدم";
-
-  const text = `*⚘اهلا و سهلا بك يا 『${taguser}』*\n\n`
-             + `*↯رقم المطور:『 [رابط رقم المطور](https://wa.me/message/RYOUR2E5SRFOL1) 』*\n\n`
-             + `*↯انستا المطور:『 [رابط انستا المطور](https://www.instagram.com/gojosaturo_876?igsh=amxrYmMydTh0NDN3) 』*`;
-
-  this.sendMessage(m.chat, text, { MessageType: 'text' });
-
-  const imageUrl = "https://telegra.ph/file/f416b40e7f5e312d80edb.jpg";
-  const imageCaption = "صورة المطور";
-  this.sendImage(m.chat, imageUrl, imageCaption);
-
-  this.sendContact(m.chat, contacts);
-}
-
-function sendMessageToContact(contactId, message) {
-  this.sendMessage(contactId, message);
 }
 
 handler.help = ['owner']
