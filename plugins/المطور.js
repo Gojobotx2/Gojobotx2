abe53c58e902327c5a2d8f75c12290611fa8605d
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-14 22:43:48 +0100

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -1,23 +1,11 @@
-const puppeteer = require('puppeteer');
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://web.whatsapp.com/');
+}
 
-    // انتظر حتى يتم تحميل صفحة واجهة واتساب
-    await page.waitForSelector('._1awRl');
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'مطور', 'المطور', 'المالك'] 
 
-    // استمع للرسائل الواردة واستجابة عند استلام ".المطور"
-    page.on('message', async (msg) => {
-        if (msg === '.المطور') {
-            // إرسال رسالة تحتوي على جهة الاتصال المحددة
-            await page.evaluate((contactNumber) => {
-                const inputField = document.querySelector('.pluggable-input-body');
-                inputField.textContent = contactNumber;
-                const sendButton = document.querySelector('._4sWnG');
-                sendButton.click();
-            }, '21653794930'); // استبدل هنا بالرقم الذي تريد إرساله
-        }
-    });
-})();
+export default handler
