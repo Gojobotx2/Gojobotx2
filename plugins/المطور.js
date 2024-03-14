11b47f1364d6c647ba97d45d3adba064d2dbf8a8
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-14 23:36:08 +0100

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -2,10 +2,14 @@ function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator);
   const contacts = data.map(([id, name]) => [id, name]);
 
-  const text = "*⚘اهلا و سهلا بك يا 『』*\n\n"
-             + "*↯رقم المطور:『 [رابط رقم المطور](https://wa.me/message/RYOUR2E5SRFOL1) 』*\n\n"
-             + "*↯انستا المطور:『 [رابط انستا المطور](https://www.instagram.com/gojosaturo_876?igsh=amxrYmMydTh0NDN3) 』*";
-  this.sendMessage(m.chat, text, MessageType.text);
+  const taguser = "المستخدم"; // تعريف متغير taguser
+
+  const text = `*⚘اهلا و سهلا بك يا 『${taguser}』*\n\n`
+             + `*↯رقم المطور:『 [رابط رقم المطور](https://wa.me/message/RYOUR2E5SRFOL1) 』*\n\n`
+             + `*↯انستا المطور:『 [رابط انستا المطور](https://www.instagram.com/gojosaturo_876?igsh=amxrYmMydTh0NDN3) 』*`;
+
+  // استخدام MessageType.text بشكل صحيح
+  this.sendMessage(m.chat, text, { MessageType: 'text' });
 
   const imageUrl = "https://telegra.ph/file/f416b40e7f5e312d80edb.jpg";
   const imageCaption = "صورة المطور";
