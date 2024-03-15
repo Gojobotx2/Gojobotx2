5a66e90df5d1963cee7ffc054b377313ce94aff6
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-15 13:46:49 +0100

    Update and rename menus-menu.js to كومندز.js

--- plugins/كومندز.js
+++ plugins/كومندز.js
@@ -133,7 +133,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['المهام'] 
+handler.command = ['كومندز'] 
 
 export default handler
 function clockString(ms) {
